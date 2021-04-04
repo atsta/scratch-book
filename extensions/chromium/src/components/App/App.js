815203da156f854165b8600bc731efe23cbccc5e
Petros Pathoulas <petros.pathoulas@di.uoa.gr>
2021-04-04 23:19:33 +0300

    [extensions] Create skeleton

---
+++ extensions/chromium/src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+/**
+ *
+ */
+export default function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Extension v0.0.1</p>
+            </header>
+        </div>
+    );
+};
