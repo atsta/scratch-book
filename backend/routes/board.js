6badb90a105d58100588e09d67955763828b8691
Athina Stamatiou <atstamatiou@gmail.com>
2021-05-30 14:21:27 +0300

    unfollow, share board

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -149,6 +149,44 @@ router.post('/follow', async (req, res) => {
     }
 });
 
+//unfollow a board
+router.post('/unfollow', async (req, res) => {    
+    const verified = verify(req, res);
+
+    try {
+        //check if title already exists
+        const board = await Board.findOne({ title: req.body.title });
+
+        if (!board)
+            return res.json({ message: "Board does not exist" });
+
+        const user = await User.findOne({_id: verified.sub});
+
+        const followed_boards = user.followed;
+
+        if (!followed_boards.includes(board._id)) 
+            return res.json({ message: "User does not follow this board" });
+                
+        for (i = 0; i < followed_boards.length; i++) {
+            if (followed_boards[i].equals(board._id)) {
+                followed_boards.splice(i, 1); 
+                break;
+            }
+        }
+
+        await User.updateOne(
+            { _id: verified.sub },  
+            {  followed : followed_boards }
+        );
+        
+        return res.json({ board: board._id, 
+                    user: verified.sub });
+        
+    } catch(err) {
+        return res.json({ error: err });
+    }
+});
+
 //boards followed by a user
 router.get('/followed', async (req, res) => {
     const verified = verify(req, res);
@@ -222,6 +260,38 @@ router.get('/shared', async (req, res) => {
     }
 });
 
+//share a board with a user 
+router.put('/share/:boardId', async (req, res) => {
+    const verified = verify(req, res);
+
+    try {
+        const user = await User.findOne({ _id: verified.sub });
+        if (!user.owned)
+            return res.json({ message: "Does not own any board" });
+        
+        const receiver = await User.findOne({ email: req.body.email });
+        if (receiver == null)
+            return res.json({ message: "Receiver user does not exist" });
+        
+        if(receiver.shared_with.includes(req.params.boardId))
+            return res.json({ message: "Board already shared with this user" });
+
+        board = await Board.findOne({ _id: req.params.boardId });
+        if (board == null) 
+            return res.json({ message: "Board does not exist" });
+    
+        await User.updateOne(
+            { _id: receiver._id },  
+            { $push: { shared_with : board._id }}
+        );
+
+        return res.json({ board_shared: req.params.boardId });
+
+    } catch (err) {
+        return res.json({ message: err });
+    }
+});
+
 //get public boards
 router.get('/public', async (req, res) => {
     const verified = verify(req, res);
@@ -245,7 +315,6 @@ router.get('/search', async (req, res) => {
         const search_result = [];
         const public_boards = await Board.find({ is_public: true });
         for(var board of public_boards) {
-            //to do - case insensitive
             if(board.title.includes(title_param)) {
                 search_result.push(board);
             }
